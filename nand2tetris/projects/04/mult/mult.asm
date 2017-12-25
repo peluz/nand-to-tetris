a41c867e7b7db11e786b74f35b507b36c69147d9
Pedro Henrique Luz de Araujo <pedrohluzaraujo@gmail.com>
2017-12-25 12:13:35 -0200

    made mult

--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,31 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+	@i
+	M=0 // set i to 0
+	@R2
+	M=0 // set R2 to 0
+
+(LOOP)
+	@i
+	D=M // D=i
+	@R1
+	D=D-M // D=i-R1
+	@END
+	D;JGE // if i >= R1 goto end
+
+	@R2
+	D=M // D=R2
+	@R0
+	D=D+M // R2+=R0
+	@R2
+	M=D // R2=D
+	@i
+	M=M+1 // i++
+	@LOOP
+	0;JMP // goto loop
+
+(END)
+	@END
+	0;JMP // Finish execution
