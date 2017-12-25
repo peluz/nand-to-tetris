ac5f5f4a7aae4c38b87d174570c5d3d0b053a6b2
Pedro Henrique Luz de Araujo <pedrohluzaraujo@gmail.com>
2017-12-25 13:08:34 -0200

    Complete project 4

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,48 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(START)
+	@SCREEN
+	D=A // D=endereço da tela
+	@pixels
+	M=D // pixels=endereço da tela
+	@KBD
+	D=M // D= tecla apertada
+
+	@LOOP_BLACK
+	D;JNE // Se tecla apertada goto loop_black
+
+	
+
+	(LOOP_WHITE)
+		@KBD
+		D=A // D=endereço de keyboard
+		@pixels
+		D=D-M // D=endereço de keyboard - endereço de pixels
+		@START
+		D;JLE // se pixels for maior q kbd pula pro inicio
+
+		@pixels
+		A=M 
+		M=0 // 16 pixels ficam brancos
+		@pixels
+		M=M+1 // endereço dos pixels anda
+		@LOOP_WHITE
+		0;JMP // volta pro loop branco
+
+	(LOOP_BLACK)
+		@KBD
+		D=A // D=endereço de keyboard
+		@pixels
+		D=D-M // D=endereço de keyboard - endereço de pixels
+		@START
+		D;JLE // se pixels for maior q kbd pula pro inicio
+
+		@pixels
+		A=M 
+		M=-1 // 16 pixels ficam pretos
+		@pixels
+		M=M+1 // endereço dos pixels anda
+		@LOOP_BLACK
+		0;JMP // volta pro loop preto
